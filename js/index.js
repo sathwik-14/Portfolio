28e8e41463bdad2a44dd1d3c026218ab50ec9616
sathwik.w <sathwik.w@pacewisdom.com>
2023-11-26 16:02:18 +0530

    updated-portfolio

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
-const nighttheme=document.getElementsByClassName('fa-moon')[0];
-const daytheme=document.getElementById('sun');
-const navbar=document.getElementsByClassName('navbar')[0];
-const body=document.getElementById('body')
-nighttheme.addEventListener('click',function(){
-daytheme.classList.toggle("d-none")
-nighttheme.classList.toggle("d-none")
-navbar.classList.replace("navbar-light", "navbar-dark");
-navbar.classList.replace("bg-light","bg-dark")
-daytheme.classList.add('text-white')
-body.classList.toggle('body')
+const nighttheme = document.getElementsByClassName("fa-moon")[0];
+const daytheme = document.getElementById("sun");
+const navbar = document.getElementsByClassName("navbar")[0];
+const body = document.getElementById("body");
+let theme = "dark";
 
+nighttheme.addEventListener("click", function () {
+  localStorage.setItem("theme", "dark");
+  daytheme.classList.toggle("d-none");
+  nighttheme.classList.toggle("d-none");
+  navbar.classList.replace("navbar-light", "navbar-dark");
+  navbar.classList.replace("bg-light", "bg-dark");
+  daytheme.classList.add("text-white");
+  body.classList.toggle("body");
 });
-daytheme.addEventListener('click',function(){
-    daytheme.classList.toggle("d-none")
-    nighttheme.classList.toggle("d-none")
-    navbar.classList.replace("navbar-dark", "navbar-light");
-navbar.classList.replace("bg-dark","bg-light")
-body.classList.toggle('body')
+daytheme.addEventListener("click", function () {
+  localStorage.setItem("theme", "light");
+  daytheme.classList.toggle("d-none");
+  nighttheme.classList.toggle("d-none");
+  navbar.classList.replace("navbar-dark", "navbar-light");
+  navbar.classList.replace("bg-dark", "bg-light");
+  body.classList.toggle("body");
+});
 
-    });
+if (localStorage.getItem("theme")) {
+  theme = localStorage.getItem("theme");
+  if (theme == "dark") nighttheme.click();
+} else {
+  nighttheme.click();
+}
